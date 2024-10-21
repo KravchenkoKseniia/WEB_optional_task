e6f6280041d0049d730cfb04f47d4a10b442d448
Ksenia Kravchenko <kkravchenko@kse.org.ua>
2024-10-21 21:41:06 +0300

    Created a dynamic page with jokes

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Happy developing ✨')
+document.getElementById('submit').addEventListener('click', async () => {
+    const numberOfJokes = parseInt(document.getElementById('input').value, 10);
+    const output = document.getElementById('output');
+
+    if (numberOfJokes === 0) {
+        output.innerHTML = `<p>Please enter a number greater than 0.</p>`;
+    }
+
+    if (numberOfJokes > 0) {
+        try {
+            const response = await fetch(`https://official-joke-api.appspot.com/jokes/random/${numberOfJokes}`);
+            const data = await response.json();
+            const jokes = data.map(joke => render(joke.setup, joke.punchline));
+
+            const container = document.body.querySelector('.output-container');
+            container.innerHTML = jokes.join("");
+
+        } catch (error) {
+            output.innerHTML = `<p>Something went wrong. Please try again later.</p>`;
+        }
+    }
+    else {
+        output.innerHTML = `<p>Please enter a valid number.</p>`;
+    }
+});
+
+function render(setup, punchline) {
+    return `
+        <div class="joke-div">
+            <p class="quote">${setup}</p>
+            <p>${punchline}</p>
+        </div>
+    `
+}
